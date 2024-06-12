ee25e9e97d0daba6cf20a297f673e726d9ad3671
Chamsron <emmacameron219@gmail.com>
2024-06-12 13:45:50 +0100

    adding enter cave method

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,42 @@ public class Main {
         System.out.println();
         System.out.println();
         startGame();
+
+    }
+
+    public static void exploreCave() {
+        System.out.println("You enter the dark cave and discover a treasure chest! Do you:");
+        System.out.println("1. Open the chest");
+        System.out.println("2. Leave the cave");
+        System.out.println("Enter your choice traveller:");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice = scanner.nextInt();
+        System.out.println();
+        System.out.println();
+
+        if (choice == 1) {
+            int coin = exploreRandom();
+            if(coin == 0) {
+                System.out.println("Turns out the chest was a mimic.");
+                System.out.println("Should have checked whether the chest was breathing!");
+                System.out.println("You died!");
+                System.out.println("Score: " + score);
+                pause();
+            } else {
+                System.out.println("You find treasure! Yippee!");
+                score = score + 20;
+                System.out.println("Your score is now: " + score);
+            }
+            } else if (choice ==2 ) {
+            System.out.println("You leave the cave and continue your adventure. You live another day traveller.");
+            confir();
+        }
+        else {
+            System.out.println("Traveller, you must be confused. Rest a while and try again.");
+        }
+        scanner.close();
     }
 
     public static void startGame() {
