a8f85af3f2b7655c3210aaddc33b2ba227013a80
Chamsron <emmacameron219@gmail.com>
2024-06-12 11:41:54 +0100

    added main class ith varibales

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static int health = 100;
+    //    Health variable to calculate health or int damage
+    public static int score = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Dungeons And Text Screens!");
+        System.out.println();
+        System.out.println();
+    }
 }
